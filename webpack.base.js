b2e0618cb2a40ddc9166322b5d4607511bee0c8f
Pablo <pfranco@outlook.com>
2017-11-03 21:00:41 +0100

    eslint configured

--- webpack.base.js
+++ webpack.base.js
@@ -3,26 +3,26 @@ const path = require('path');
 const sourcePath = path.resolve(__dirname, 'source');
 
 module.exports = {
-    entry: './source/main.js',
-    output: {
-        path: path.resolve(__dirname, 'built'),
-        filename: 'app.js',
-    },
+  entry: './source/main.js',
+  output: {
+    path: path.resolve(__dirname, 'built'),
+    filename: 'app.js',
+  },
 
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': sourcePath
-        }
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': sourcePath,
     },
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: sourcePath,
-                loader: 'babel-loader',
-            },
-        ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: sourcePath,
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,14 @@
-const path                  = require('path');
-const merge                 = require('webpack-merge');
-const webpackBasicConfig    = require('./webpack.base.js');
+const path = require('path');
+const merge = require('webpack-merge');
+const webpackBasicConfig = require('./webpack.base.js');
 
 module.exports = merge(webpackBasicConfig, {
-    devtool: '#source-map',
-    
-    devServer: {
-        contentBase: path.resolve(__dirname, 'built/'),
-        staticOptions: {
-            redirect: false
-        }
+  devtool: '#source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'built/'),
+    staticOptions: {
+      redirect: false,
     },
+  },
 
 });
